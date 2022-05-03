55eefb4ff43c3159142cb44d3f418b2a2c1656be
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-03 06:31:42 +0530

    Update terraform/terraform-loadbalancers/

--- main.tf
+++ main.tf
@@ -2,11 +2,13 @@ module "alb-public" {
   source   = "./vendor/modules/alb"
   ALB_NAME = "roboshop-public"
   INTERNAL = false
+  ENV      = var.ENV
 }
 
 module "alb-private" {
   source   = "./vendor/modules/alb"
   ALB_NAME = "roboshop-private"
   INTERNAL = true
+  ENV      = var.ENV
 }
 
