1ab362d32a9280202ff1dd26c3391bed819a5ad7
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-05 06:09:17 +0530

    Update terraform/terraform-loadbalancers/

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,12 @@ output "PRIVATE_ALB_ARN" {
 output "PRIVATE_LISTENER_ARN" {
   value = module.alb-private.LISTENER_ARN[0]
 }
+
+output "PRIVATE_ALB_ADDRESS" {
+  value = module.alb-private.ALB_ADDRESS
+}
+
+output "PUBLIC_ALB_ADDRESS" {
+  value = module.alb-public.ALB_ADDRESS
+}
+
